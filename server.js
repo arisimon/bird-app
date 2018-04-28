71889cf803784e88df7bc00962e172765033c0fa
Ari Simon <arisimon2@gmail.com>
2018-04-28 16:00:54 -0700

    updated dependencies and server files

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs')
 const path = require('path');
 const exphbs  = require('express-handlebars');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//set bodyParser
+app.use(bodyParser.json());
+
+// set morgan to log only 4xx and 5xx responses to console
+app.use(logger('dev', {
+  skip: function (req, res) { return res.statusCode < 400 }
+}))
+
+// set morgan to log all requests to access.log
+app.use(logger('common', {
+  stream: fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+}))
 
 //require routers
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-const app = express();
 
 //set up view engine - express handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
@@ -19,10 +35,49 @@ app.set('views', path.join(__dirname, 'views'));
 //serve static assets
 app.use(express.static('public'));
 
+
 //serve routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+let server;
+
+// //run server
+// function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+// 	return new Promise((resolve, reject) => {
+// 		mongoose.connect(databaseUrl, err => {
+// 			if (err) {
+// 				return reject(err);
+// 			}
+// 			server = app.listen(port, () => {
+// 				console.log(`Your app is listening on port ${port}`);
+// 				resolve();
+// 			})
+// 				.on('error', err => {
+// 					mongoose.disconnect();
+// 					reject(err);
+// 				});
+// 		});
+// 	});
+// }
+
+// // close server, return promise
+// function closeServer() {
+// 	return mongoose.disconnect().then(() => {
+// 		return new Promise((resolve, reject) => {
+// 			console.log('Closing server');
+// 			server.close(err => {
+// 				if (err) {
+// 					return reject(err);
+// 				}
+// 				resolve();
+// 			});
+// 		});
+// 	});
+// };
+
+//if server is called directly
 if (require.main === module) {
   app.listen(process.env.PORT || 8080, function () {
     console.info(`Server started on ${this.address().port}`);
