d857ddffad63ff78760b1bc9139910ae690e4d07
Ari Simon <arisimon2@gmail.com>
2018-05-08 20:56:04 -0700

    added strict

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,14 @@
+'use strict';
+
 const SPECIES_URL = 'species';
 const OBSERVATION_URL = 'observations';
 const EBIRD_KEY = 'c57cssobo0im';
+
+
+function getObservations(callback) {
+  // getJSON call to get posts
+  $.getJSON('/observations', function(data) {
+    callbackFn(data)
+  })
+}
+
